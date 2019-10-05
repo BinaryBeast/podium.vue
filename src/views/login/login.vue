82ed38ff0b7246294423f9f2f322232d4683e8e4
Chris Dunn-Birch <chris@dunnbirch.com>
2019-10-05 04:55:34 +0100

    Implements a basic login component.

---
+++ src/views/login/login.vue
@@ -0,0 +1,30 @@
+<script>
+    const field = () => import("components/field/field.vue");
+
+    export default {
+        name: "login",
+        components: {
+            field
+        },
+    };
+</script>
+
+<template>
+    <form class="login">
+        <h1>
+            Login
+        </h1>
+
+        <field
+            label="Email address"
+            name="email"
+            type="email"
+        />
+
+        <field
+            label="Password"
+            name="password"
+            type="password"
+        />
+    </form>
+</template>
