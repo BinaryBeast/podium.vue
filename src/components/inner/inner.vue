9284396979c083f46115934f2e189efdbd27efc6
Chris Dunn-Birch <chris@dunnbirch.com>
2019-10-05 04:55:18 +0100

    Implements a wrapper component for the inner layout section.

---
+++ src/components/inner/inner.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        name: "inner",
+    };
+</script>
+
+<template>
+    <div id="inner">
+        <slot></slot>
+    </div>
+</template>
+
+<style src="./inner.scss" lang="scss"></style>
