90128ce39a3b6a3a8ef94930b94e9541129232a5
Chris Dunn-Birch <chris@dunnbirch.com>
2019-10-05 04:55:43 +0100

    Implements a basic register component.

---
+++ src/views/register/register.vue
@@ -0,0 +1,40 @@
+<script>
+    const field = () => import("components/field/field.vue");
+
+    export default {
+        name: "register",
+        components: {
+            field,
+        },
+    };
+</script>
+
+<template>
+    <form class="register">
+        <h1>
+            Register
+        </h1>
+
+        <field
+            label="Email address"
+            name="email"
+            type="email"
+        />
+        <field
+            label="Confirm email address"
+            name="confirm_email"
+            type="email"
+        />
+
+        <field
+            label="Password"
+            name="password"
+            type="password"
+        />
+        <field
+            label="Confirm password"
+            name="confirm_password"
+            type="password"
+        />
+    </form>
+</template>
