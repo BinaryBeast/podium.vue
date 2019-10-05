30344d6d6d3d30e853120fce189a8ce7177adfdb
Chris Dunn-Birch <chris@dunnbirch.com>
2019-10-05 04:54:47 +0100

    Implements a drawer component with vue-router links between 3 pages.

---
+++ src/components/drawer/drawer.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        name: "drawer",
+    };
+</script>
+
+<template>
+    <div id="drawer">
+        <router-link
+            to="/"
+            class="drawer__link"
+        >
+            Home
+        </router-link>
+
+        <router-link
+            to="/login"
+            class="drawer__link"
+        >
+            Login
+        </router-link>
+
+        <router-link
+            to="/register"
+            class="drawer__link"
+        >
+            Register
+        </router-link>
+    </div>
+</template>
+
+<style src="./drawer.scss" lang="scss"></style>
