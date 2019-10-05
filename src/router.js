bb239993a0a0d742c81b2d6cb3c252f00b92ace1
Chris Dunn-Birch <chris@dunnbirch.com>
2019-10-05 04:49:59 +0100

    Configures 3 basic routes.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+
+const home = () => import("./views/home/home.vue");
+const login = () => import("./views/login/login.vue");
+const register = () => import("./views/register/register.vue");
 
 Vue.use(Router);
 
@@ -11,15 +14,17 @@ export default new Router({
         {
             path: "/",
             name: "home",
-            component: Home
+            component: home
         },
         {
-            path: "/about",
-            name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+            path: "/login",
+            name: "login",
+            component: login
+        },
+        {
+            path: "/register",
+            name: "register",
+            component: register
         }
     ]
 });
