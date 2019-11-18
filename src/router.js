3cff52375a17add50aada585e0ff430bc77b0497
Chris Dunn-Birch <chris@dunnbirch.com>
2019-11-18 04:46:49 +0000

    Fixes indentation.

--- src/router.js
+++ src/router.js
@@ -3,28 +3,22 @@ import Router from "vue-router";
 
 const home = () => import("./views/home/home.vue");
 const login = () => import("./views/login/login.vue");
-const register = () => import("./views/register/register.vue");
 
 Vue.use(Router);
 
 export default new Router({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: home
-        },
-        {
-            path: "/login",
-            name: "login",
-            component: login
-        },
-        {
-            path: "/register",
-            name: "register",
-            component: register
-        }
-    ]
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: home
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: login
+    }
+  ]
 });
