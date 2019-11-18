2b8995d3f9c6b0b6721380eba85d7a4cef7665a7
Chris Dunn-Birch <chris@dunnbirch.com>
2019-11-18 04:43:27 +0000

    Starts implementation of search component.

---
+++ src/components/search/search.vue
@@ -0,0 +1,36 @@
+<script>
+  const icon = () => import("components/icon/icon.vue");
+
+  export default {
+    name: "search",
+    components: {
+      icon
+    },
+  };
+</script>
+
+<template>
+  <form class="search">
+    <label
+      for="search_term"
+      class="search__label"
+    >
+      <input
+        id="search_term"
+        class="search__input"
+        name="search_term"
+        type="text"
+        placeholder="test"
+      />
+    </label>
+
+    <button
+      class="search__submit"
+      type="submit"
+    >
+      <icon name="login" />
+    </button>
+  </form>
+</template>
+
+<style src="./search.scss" lang="scss"></style>
