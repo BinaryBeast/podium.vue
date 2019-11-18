386cdde181de974c00885261f1ae510c73c6bf6d
Chris Dunn-Birch <chris@dunnbirch.com>
2019-11-18 04:43:44 +0000

    Starts implementation of button component.

---
+++ src/components/button/button.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+    name: "button",
+    props: {
+      caption: {
+        type: String,
+        default: ""
+      },
+    },
+  };
+</script>
+
+<template>
+  <button
+    class="button"
+    type="button"
+  >
+    <span class="button__caption">
+      {{ caption }}
+    </span>
+  </button>
+</template>
+
+<style src="./button.scss" lang="scss"></style>
