541c3704e2275d63d538bfba35e7d9c18383873c
Chris Dunn-Birch <chris@dunnbirch.com>
2019-11-18 04:46:20 +0000

    Adds masthead component to app entry point.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <script>
-    import "typeface-overpass";
+  import "typeface-nunito-sans";
 
-    const drawer = () => import("components/drawer/drawer.vue");
-    const inner = () => import("components/inner/inner.vue");
+  const masthead = () => import("components/masthead/masthead.vue");
+  const drawer = () => import("components/drawer/drawer.vue");
+  const inner = () => import("components/inner/inner.vue");
 
-    export default {
-        name: "app",
-        components: {
-            drawer,
-            inner,
-        },
-    };
+  export default {
+    name: "app",
+    components: {
+      masthead,
+      drawer,
+      inner,
+    },
+  };
 </script>
 
 <template>
-    <div id="podium">
-        <drawer />
+  <div id="podium">
+    <masthead />
 
-        <inner>
-            <router-view />
-        </inner>
-    </div>
+    <drawer />
+
+    <inner>
+      <router-view />
+    </inner>
+  </div>
 </template>
 
 <style src="styles/podium.scss" lang="scss"></style>
