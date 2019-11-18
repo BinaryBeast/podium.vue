d84527075c22b696e7beb5bebe36517063b0a0dc
Chris Dunn-Birch <chris@dunnbirch.com>
2019-11-18 04:38:01 +0000

    Configures Webpack via Vue configuration.
    * Adds assets, components, and styles as Webpack aliases for simplified absolute imports.
    * Forces Webpack to use vue-svg-inline-loader for .svg img elements.
    * Auto-imports _base.scss to every <style> in .vue components.

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,33 @@
 const path = require("path");
 
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                "components": path.resolve(__dirname, "src/components"),
-                "styles": path.resolve(__dirname, "src/styles"),
-            },
-        },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "assets": path.resolve(__dirname, "src/assets"),
+        "components": path.resolve(__dirname, "src/components"),
+        "styles": path.resolve(__dirname, "src/styles"),
+      },
     },
-    css: {
-        loaderOptions: {
-            sass: {
-                data: "@import 'styles/_base.scss';",
-            },
-        },
+  },
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-svg-inline-loader")
+      .loader("vue-svg-inline-loader")
+      .options({
+        removeAttributes: [
+          "alt",
+          "src",
+          "svg-inline"
+        ]
+      });
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: "@import 'styles/_base.scss';",
+      },
     },
+  },
 };
