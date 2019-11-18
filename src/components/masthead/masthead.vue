1ae3e50e08e1509b3bb27ca4f4b142469db040a2
Chris Dunn-Birch <chris@dunnbirch.com>
2019-11-18 04:43:14 +0000

    Starts implementation of masthead component.

---
+++ src/components/masthead/masthead.vue
@@ -0,0 +1,29 @@
+<script>
+  const search = () => import("components/search/search.vue");
+
+  export default {
+    name: "masthead",
+    components: {
+      search
+    }
+  };
+</script>
+
+<template>
+  <div class="masthead">
+    <figure class="masthead__logo">
+      <router-link
+        to="/"
+        class="masthead__link"
+      >
+        podium
+      </router-link>
+    </figure>
+
+    <div class="masthead__search">
+      <search />
+    </div>
+  </div>
+</template>
+
+<style src="./masthead.scss" lang="scss"></style>
