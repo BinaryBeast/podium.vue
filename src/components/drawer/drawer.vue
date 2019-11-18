ae4d6b7a47958430f54eb6dd27b57fbc0e5b3bea
Chris Dunn-Birch <chris@dunnbirch.com>
2019-11-18 04:40:19 +0000

    Fixes indentation and removes Register link in Drawer component.

--- src/components/drawer/drawer.vue
+++ src/components/drawer/drawer.vue
@@ -1,32 +1,25 @@
 <script>
-    export default {
-        name: "drawer",
-    };
+  export default {
+    name: "drawer",
+  };
 </script>
 
 <template>
-    <div id="drawer">
-        <router-link
-            to="/"
-            class="drawer__link"
-        >
-            Home
-        </router-link>
+  <div id="drawer">
+    <router-link
+      to="/"
+      class="drawer__link"
+    >
+      Home
+    </router-link>
 
-        <router-link
-            to="/login"
-            class="drawer__link"
-        >
-            Login
-        </router-link>
-
-        <router-link
-            to="/register"
-            class="drawer__link"
-        >
-            Register
-        </router-link>
-    </div>
+    <router-link
+      to="/login"
+      class="drawer__link"
+    >
+      Login with Scatter
+    </router-link>
+  </div>
 </template>
 
-<style src="./drawer.scss" lang="scss"></style>
+<style src="./drawer.scss" lang="scss" scoped></style>
